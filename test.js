cecbd8d4585ab68cf23bd0deaa0ed2de8974fc37
Vitor Mattos <vitor.cavalcanti@olxbr.com>
2021-06-15 13:59:34 -0300

    wraps test in try/catch

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const {
   buildImage,
   pushImage,
   tagImage,
+  assumeRole,
 } = require('./main')
 
 const REPO = 'cross/devtools/momo'
@@ -10,20 +11,30 @@ const TAGS = '0.2.2,beta'
 
 
 const test = async () => {
-  const tags = TAGS.split(',')
+  try {
+    const tags = TAGS.split(',')
 
-  const params = {
-    repositoryNames: [REPO],
-    tags,
+    const params = {
+      repositoryNames: [REPO],
+      tags,
+    }
+
+    console.log('Assuming role')
+    await assumeRole({
+      RoleArn: 'arn:aws:iam::073521391622:role/github_actions_integration_role',
+      RoleSessionName: 'test'
+    })
+
+    const output = await getRepositoryUri(params)
+    console.log(output.repositories[0].repositoryUri)
+    await buildImage(params)
+    tags.forEach(async (tag) => {
+      await tagImage({ ...params, tag })
+    })
+    await pushImage(params).catch(console.error)
+  } catch(e) {
+    console.log(e)
   }
-
-  const output = await getRepositoryUri(params)
-  console.log(output.repositories[0].repositoryUri)
-  await buildImage(params)
-  tags.forEach(async (tag) => {
-    await tagImage({ ...params, tag })
-  })
-  await pushImage(params).catch(console.error)
 }
 
 test()
